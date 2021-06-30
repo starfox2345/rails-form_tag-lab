bf55e49cc78de5ac12636cc39e7ab717391f7e42
yatsun <yatsunlam25@gmail.com>
2021-06-29 20:54:59 -0400

    commit tree 95202bd721e7baa3435bbf08a8e73c2e746d7f3b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,11 +8,22 @@ class StudentsController < ApplicationController
     @student = Student.find_by_id(params[:id])
   end
 
+  def new
+
+  end
  
 
   def create
-    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
-    redirect_to students_path
+    # @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    # redirect_to student_path(@student)
+    session[:form_params] = params.inspect
+    redirect_to new_student_path
   end
+
+  # helpers do
+  #   def find_student
+  #     @student = Student.find_by_id(params[:id])
+  #   end
+  # end
 
 end
